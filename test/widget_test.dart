a6a7b300873efd2e37d1d143ddf55c4f5b88447a
MostafaElzohirey <132284280+Mostafa-Elzohirey@users.noreply.github.com>
2025-03-06 01:30:26 +0200

    lang JSONs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:workify/main.dart';
+import 'package:workify/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
