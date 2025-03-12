5091bee99a51530f8cfb403c6e10ea07110951cc
MahmoudSobhiAwad <sobhimahmoud003@gmail.com>
2025-03-13 01:17:31 +0200

    add logout feature.

--- lib/shared/features/settings/presentation/pages/settings_view.dart
+++ lib/shared/features/settings/presentation/pages/settings_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:workify/core/storage/cache_helper.dart';
 import 'package:workify/core/utils/theme/app_colors.dart';
 import 'package:workify/core/utils/theme/app_font_stlyles.dart';
 import 'package:workify/core/utils/theme/app_icons.dart';
 import 'package:workify/shared/features/settings/presentation/widgets/custom_settings_item.dart';
-
 
 class SettingView extends StatelessWidget {
   const SettingView({super.key});
@@ -72,6 +72,9 @@ class SettingView extends StatelessWidget {
             height: 30,
           ),
           CustomSettingsItem(
+            onTap: () async {
+              await AppSharedPreferences.clearAll();
+            },
             label: 'Log out',
             iconPath: AppIcons.assetsIconsLogOutIcon,
           )
